b89acb447aedbe44970445beeebdc556153d2350
Fredrik August Madsen-Malmo <mail.fredrikaugust@gmail.com>
2016-04-13 19:11:35 +0200

    changed to use gtk2

---
+++ cg_gui.rb
@@ -0,0 +1,86 @@
+#!/home/greg/.rvm/rubies/default/bin/ruby
+
+require 'gtk2'
+require './color_guesser'
+
+# Setup login for ColorGuesser
+guesser = ColorGuesser::ColorGuesser.new
+
+# Create window
+window = Gtk::Window.new
+
+window.title = "Color Thing"
+window.border_width = 10
+
+# Buttons
+button_light = Gtk::Button.new("_Light")
+button_dark = Gtk::Button.new("_Dark")
+button_guess = Gtk::Button.new("_Guess")
+
+# Image
+img_color = Gtk::Image.new("square.png")
+
+# Main box (V)
+main_box = Gtk::VBox.new(false, 0)
+
+# Button box (H)
+button_box = Gtk::HBox.new(false, 0)
+
+# Image color text
+text_color = Gtk::Label.new(guesser.current_color)
+
+# Guess text
+text_guess = Gtk::Label.new('Please answer some colors before using the guess function')
+
+# Hooks
+window.signal_connect("destroy") {
+  Gtk.main_quit
+}
+
+def choose(type, guesser_obj, img, guess)
+  guesser_obj.add_color(guesser_obj.current_color, type)
+  # reload image
+  img.file = "square.png"
+  # hide the guessed answer
+  guess.hide
+end
+
+# dark color selected
+button_dark.signal_connect("clicked") do
+ choose("dark", guesser, img_color, text_guess)
+end
+
+# light color selected
+button_light.signal_connect("clicked") do
+ choose("light", guesser, img_color, text_guess)
+end
+
+button_guess.signal_connect("clicked") do
+  _color = ColorGuesser::Color.new(guesser.current_color)
+  text_guess.text = "My guess is: #{guesser.decide(_color)}.\n\
+Difference: \
+#{guesser.color_chance(_color).map{|k,v|"#{k}: #{v.to_i}"}}"
+  text_guess.show_now
+end
+
+# Add elems
+window.add(main_box)
+
+# add the buttons
+button_box.pack_start(button_light, true, true, 0)
+button_box.pack_start(button_dark, true, true, 0)
+
+# image with color
+main_box.pack_start(img_color)
+# color text
+main_box.add(text_color)
+# light/dark buttons
+main_box.pack_start(button_box)
+# guess button
+main_box.pack_start(button_guess, true, true, 0)
+# guess text
+main_box.pack_start(text_guess, true, true, 0)
+
+# Start!
+window.show_all
+Gtk.main
---
+++ color_guesser.rb
@@ -0,0 +1,97 @@
+#!/home/greg/.rvm/rubies/default/bin/ruby
+
+require 'rmagick'
+
+class String
+  def convert_base(from, to)
+    to_i(from).to_s(to)
+  end
+end
+
+class Numeric
+  def diff(other_value)
+    (self - other_value).to_f.abs
+  end
+end
+
+class Array
+  def mean
+    inject(&:+) / size # sum / length of self
+  end
+end
+
+module ColorGuesser
+  class ColorSquare
+    def initialize(color)
+      imgl = Magick::ImageList.new
+      imgl.new_image(200,200, Magick::HatchFill.new(color, color))
+      imgl.write("square.png")
+    end
+  end
+
+  class Color
+    attr_reader :hex_color, :rgb
+
+    # when printing the current color
+    def to_s
+      "#{@hex_color}"
+    end
+
+    def initialize(rgb_s)
+      @hex_color = rgb_s
+      # remove '#' and split into arrays of 2
+      rgb_a = rgb_s.sub('#','').split('').each_slice(2).to_a
+      # convert each num from base16 to base10
+      rgb_a.map! {|x| x.join('').convert_base(16,10).to_i}
+
+      @rgb = {r: rgb_a[0], g: rgb_a[1], b: rgb_a[2]}
+    end
+
+    def self.random_color
+      _rand_color = "#" + rand("ffffff".convert_base(16,10).to_i)
+                            .to_s.convert_base(10,16)
+      # because it removes trailing 0's
+      _rand_color += "0" until _rand_color.length == 7
+      _rand_color
+    end
+  end
+
+  class ColorGuesser
+    attr_accessor :light, :dark, :current_color
+
+    def initialize
+      # instanciate empty arrs
+      @light, @dark = {r: [], g: [], b:[]}, {r: [], g: [], b:[]}
+      @current_color = Color.random_color
+      ColorSquare.new @current_color
+    end
+
+    def add_color(color, type)
+      # create a local color object
+      _color = Color.new(color).rgb
+      # add the R from color to the R arr in self.white|black etc. for G and B
+      _new = instance_variable_get("@#{type}").merge(_color){ |k,o,n|o << n }
+      # save the abovementioned
+      instance_variable_set("@#{type}", _new)
+      @current_color = Color.random_color
+      ColorSquare.new @current_color
+    end
+
+    def color_chance(color)
+      ["dark", "light"].map do |t|
+        self.instance_variable_get("@#{t}").map do |k,v| # get current color
+          target_color = color.rgb["#{k}".to_sym]
+          # difference between
+          # mean of (mean color value + the closest color in dark/light)
+          # closest color in dark/light
+          [v.mean, v.min_by{|x|(x-target_color).abs}].mean.diff(target_color)
+        end.mean
+      end.zip([:dark, :light]).map(&:reverse).to_h # convert back to hash
+    end
+
+    def decide(color)
+      # get the type with the smallest total diff from the provided color
+      color_chance(color).min_by{ |k,v| v }.first.to_s
+    end
+  end
+end
